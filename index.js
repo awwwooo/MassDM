84f65a3b5fc7b2c38ede532dd89d1ab0188dca12
awwwooo <45100277+awwwooo@users.noreply.github.com>
2018-11-19 10:48:49 -0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(100);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 270000);
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let mensagem = ""; // Mensagem que será enviada no privado, caso deseje faezr uma quebra de linha utilize '\n'
+let msg = ""; 
 let token = ""; // Token
 
 client.on('ready', () => {
@@ -25,7 +12,7 @@ client.on('ready', () => {
         if(member.user.bot) return;
         if(member.hasPermission("BAN_MEMBERS") || member.hasPermission("KICK_MEMBERS") || member.hasPermission("MANAGE_ROLES")) return;
         console.log(`Enviando mensagem para ${member.user.username}`);
-        member.send(`${mensagem}`);
+        member.send(`${msg}`);
     }, 30000);
     });
 })
